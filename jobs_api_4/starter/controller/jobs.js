363bff26f162b1fc2ba9aad4873bc22178ac37a9
kumar harsh <hk995814@gmail.com>
2025-08-19 00:01:41 +0530

    working on single route to create jobs

--- jobs_api_4/starter/controller/jobs.js
+++ jobs_api_4/starter/controller/jobs.js
@@ -1,3 +1,8 @@
+const Job = require("../models/job")
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError,NotFoundError} = require('../errors')
+
+
 const getAllJobs = async(req,res)=>{
     res.send('get all jobs')
 }
@@ -6,7 +11,10 @@ const getJob = async(req,res)=>{
     res.send('get jobs')
 }
 const createJob = async(req,res)=>{
-    res.json(req.user)
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
+   
 }
 
 const updateJob = async(req,res)=>{
