56df28286bd438512cad6e37438eccc822554053
kumar harsh <hk995814@gmail.com>
2025-08-20 00:05:42 +0530

    work on routing

--- jobs_api_4/starter/controller/jobs.js
+++ jobs_api_4/starter/controller/jobs.js
@@ -4,11 +4,21 @@ const {BadRequestError,NotFoundError} = require('../errors')
 
 
 const getAllJobs = async(req,res)=>{
-    res.send('get all jobs')
+    const jobs =await Job.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs, count:jobs.length})
 }
 
 const getJob = async(req,res)=>{
-    res.send('get jobs')
+  const {user:{userId},params:{id:jobId}} = req
+
+  const job = await Job.findOne({
+    _id:jobId,createdBy:userId
+  })
+ if(!job){
+    throw new NotFoundError(`No job with id ${jobId}`)
+ }
+ res.status(StatusCodes.OK).json({job})
+
 }
 const createJob = async(req,res)=>{
     req.body.createdBy = req.user.userId
